8944ae33ef55b947df43296807bfe2b68fb3fb95
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-23 12:54:27 +0300

    Initial Commit

---
+++ MovieBook(JSON-RestAPI)SwiftUI/MovieBook_JSON_RestAPI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBook_JSON_RestAPI_SwiftUIApp.swift
+//  MovieBook(JSON-RestAPI)SwiftUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 23.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieBook_JSON_RestAPI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
